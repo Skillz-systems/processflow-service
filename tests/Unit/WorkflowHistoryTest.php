5ba4d25fab082eb0705ed146d5769c997bcd007b
Victor Akwara <victorakwara83@gmail.com>
2024-01-24 14:46:36 +0100

    fixed

--- tests/Unit/WorkflowHistoryTest.php
+++ tests/Unit/WorkflowHistoryTest.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Http\Request;
 use Tests\TestCase;
 
-class WorkflowControllerTest extends TestCase
+class WorkflowHistoryTest extends TestCase
 {
     use RefreshDatabase;
 
@@ -29,7 +29,7 @@ class WorkflowControllerTest extends TestCase
         ]);
         $createNewWorkflowHistoryService = new WorkflowHistoryService();
         $createNewWorkflowHistory = $createNewWorkflowHistoryService->createWorkflowHistory($data);
-        $this->assertDatabaseHas('work_flow_history', [
+        $this->assertDatabaseHas('workflow_histories', [
             "user_id" => 1,
             "task_id" => 1,
             "step_id" => 1,
@@ -40,8 +40,8 @@ class WorkflowControllerTest extends TestCase
         $this->assertInstanceOf(WorkflowHistory::class, $createNewWorkflowHistory);
 
         $this->assertNotNull($createNewWorkflowHistory->id);
-        $this->assertSame('user id', $createNewWorkflowHistory->user_id);
-        $this->assertSame('task id', $createNewWorkflowHistory->task_id);
+        $this->assertSame(1, $createNewWorkflowHistory->user_id);
+        $this->assertSame(1, $createNewWorkflowHistory->task_id);
     }
 
     public function test_to_see_if_an_error_happens_when_creating_a_workflowHistory(): void
