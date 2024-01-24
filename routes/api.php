45188631c9111c6abfcc0dcf25cb9f1331568429
Victor Akwara <victorakwara83@gmail.com>
2024-01-24 07:47:17 +0100

    workflow history with test

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::post('/create-workflow-history', [WorkflowController::class, 'createWorkflowHistory']);
