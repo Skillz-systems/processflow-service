5e39cf04c3423f9038c4afc41410fce7f639c7a5
Collins Nwoko C <collinsnwoko05@gmail.com>
2024-01-31 20:52:50 +0100

    swagger documentation

--- app/Http/Controllers/ProcessFlowController.php
+++ app/Http/Controllers/ProcessFlowController.php
@@ -10,6 +10,10 @@ use App\Service\ProcessflowStepService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
+/**
+ * @OA\Tag(name="Process Flows")
+ */
+
 class ProcessFlowController extends Controller
 {
     /**
@@ -42,6 +46,24 @@ class ProcessFlowController extends Controller
      *
      * @param StoreProcessFlowRequest $request The request containing the process flow data.
      * @return ProcessFlowResource The created process flow resource.
+     */
+
+    /**
+     * @OA\Post(
+     *     path="/process-flows",
+     *     tags={"Process Flows"},
+     *     summary="Store a new process flow ",
+     *     description="Stores a new process flow and associated steps if available",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(ref="#/components/schemas/StoreProcessFlowRequest")
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="Process Flow created",
+     *         @OA\JsonContent(ref="#/components/schemas/ProcessFlowResource")
+     *     )
+     * )
      */
 
     public function store(StoreProcessFlowRequest $request)
@@ -81,6 +103,27 @@ class ProcessFlowController extends Controller
      * @param string $id The ID of the process flow to retrieve.
      * @return ProcessFlowResource The process flow resource.
      */
+
+    /**
+     * @OA\Get(
+     *     path="/process-flows/{id}",
+     *     tags={"Process Flows"},
+     *     summary="Get a process flow",
+     *     description="Returns the details of a single process flow with its associated steps",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="ID of the process flow",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Process Flow found",
+     *         @OA\JsonContent(ref="#/components/schemas/ProcessFlowResource")
+     *     )
+     * )
+     */
     public function show(string $id)
     {
         $processFlow = $this->processFlowService->getProcessFlow($id);
@@ -88,8 +131,30 @@ class ProcessFlowController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
+     * @OA\Put(
+     *     path="/process-flows/{id}",
+     *     tags={"Process Flows"},
+     *     summary="Update a process flow",
+     *     description="Updates the details of an existing process flow with it's associated steps if provided",
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         description="ID of the process flow to update",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(ref="#/components/schemas/UpdateProcessFlowRequest")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Process Flow updated",
+     *         @OA\JsonContent(ref="#/components/schemas/ProcessFlowResource")
+     *     )
+     * )
      */
+
     public function update(UpdateProcessFlowRequest $request, int $id)
     {
         return DB::transaction(function () use ($request, $id) {
