d0037dca4099a7c26a75893d7e72ad886e4edc5a
Collins Nwoko C <collinsnwoko05@gmail.com>
2024-01-31 19:23:38 +0100

    update processflow with or without steps

--- app/Observers/ProcessFlowObserver.php
+++ app/Observers/ProcessFlowObserver.php
@@ -99,12 +99,12 @@ class ProcessFlowObserver
 namespace App\Observers;
 
 use App\Models\ProcessFlow;
-use App\Service\ProcessFlowService;
 use App\Service\ProcessflowStepService;
 use Illuminate\Http\Request;
 
 class ProcessFlowObserver
 {
+<<<<<<< HEAD
 <<<<<<< HEAD
     //
 >>>>>>> c717375 (added test)
@@ -113,8 +113,11 @@ class ProcessFlowObserver
 =======
 =======
     protected $processFlowService, $processflowStepService;
+=======
+    protected $processflowStepService;
+>>>>>>> 8462c4e (update processflow with or without steps)
 
-    public function __construct(ProcessFlowService $processFlowService, ProcessflowStepService $processflowStepService)
+    public function __construct(ProcessflowStepService $processflowStepService)
     {
         $this->processflowStepService = $processflowStepService;
     }
@@ -122,39 +125,12 @@ class ProcessFlowObserver
     public function updating(ProcessFlow $processFlow)
     {
         // Add logic to run before the process flow is updated
-        // $processFlowId = request()->input('id');
-        // if (request()->has('steps')) {
-
-        //     $steps = request()->input('steps');
-        //     // $steps = $processFlow->steps;
-        //     $updatedSteps = [];
-
-        //     foreach ($steps as $index => $step) {
-        //         $step = $this->processflowStepService->updateProcessFlowStep(new Request($step), $step['id']);
-
-        //         if ($index === 0) {
-        //             $this->processFlowService->updateProcessFlow(['start_step_id' => $step->id], $processFlowId);
-        //         }
-        //         $updatedSteps[] = $step;
-        //     }
-
-        //     foreach ($updatedSteps as $index => $step) {
-        //         $next_step_id = $index === count($updatedSteps) - 1 ? null : $updatedSteps[$index + 1]->id;
-        //         $this->processflowStepService->updateProcessFlowStep(new Request(['next_step_id' => $next_step_id]), $step->id);
-        //     }
-        // }
-        // else {
-        //     $this->processFlowService->updateProcessFlow($processFlow->id, $processFlow);
-
-        // }
 
     }
 
     public function updated(ProcessFlow $processFlow)
     {
         // Add logic to run after the process flow is updated
-
-        // $processFlowId = request()->input('id');
         if (request()->has('steps')) {
 
             $steps = request()->input('steps');
@@ -176,29 +152,6 @@ class ProcessFlowObserver
             }
         }
 
-        // if (request()->has('steps')) {
-
-        //     $steps = request()->input('steps');
-        //     // $steps = $processFlow->steps;
-        //     $updatedSteps = [];
-
-        //     foreach ($steps as $index => $step) {
-        //         $step = $this->processflowStepService->updateProcessFlowStep(new Request($step), $step['id']);
-
-        //         if ($index === 0) {
-        //             $this->processFlowService->updateProcessFlow(['start_step_id' => $step->id], $processFlow->id);
-        //         }
-        //         $updatedSteps[] = $step;
-        //     }
-
-        //     foreach ($updatedSteps as $index => $step) {
-        //         $next_step_id = $index === count($updatedSteps) - 1 ? null : $updatedSteps[$index + 1]->id;
-        //         $this->processflowStepService->updateProcessFlowStep(new Request(['next_step_id' => $next_step_id]), $step->id);
-        //     }
-        // } else {
-        //     $this->processFlowService->updateProcessFlow($processFlow->id, $processFlow);
-
-        // }
     }
 >>>>>>> 63b2932 (update observer)
 >>>>>>> 56b5cde (update observer)
