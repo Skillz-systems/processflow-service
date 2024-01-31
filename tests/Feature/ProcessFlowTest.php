a7791f2756882098320ee27bb1f12e1d29cee9e7
Collins Nwoko C <collinsnwoko05@gmail.com>
2024-01-31 14:28:22 +0100

    added test

--- tests/Feature/ProcessFlowTest.php
+++ tests/Feature/ProcessFlowTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\ProcessFlow;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -251,5 +252,119 @@ class ProcessFlowTest extends TestCase
         $response->assertStatus(401);
 
         $this->getJson('/api/processflows/1')->assertStatus(401);
+    }
+/***
+ * UPDATE TESTS
+ */
+    public function test_to_update_process_flow_with_valid_data_successfully(): void
+    {
+        $processFlowData = [
+            'name' => 'Test Process Flow',
+            'frequency' => 'weekly',
+            'status' => true,
+            'frequency_for' => 'users',
+            'day' => null,
+            'week' => 'monday',
+            'steps' => [
+                [
+
+                    'name' => 'test name single test',
+                    'step_route' => 'this should be a route',
+                    'assignee_user_route' => 1,
+                    'next_user_designation' => 1,
+                    'next_user_department' => 1,
+                    'next_user_unit' => 1,
+                    'next_user_location' => 1,
+                    'step_type' => 'create',
+                    'user_type' => 'customer',
+                    'status' => 1,
+                ],
+                [
+
+                    'name' => 'test name single two test',
+                    'step_route' => 'this should be a route',
+                    'assignee_user_route' => 1,
+                    'next_user_designation' => 1,
+                    'next_user_department' => 1,
+                    'next_user_unit' => 1,
+                    'next_user_location' => 1,
+                    'step_type' => 'create',
+                    'user_type' => 'customer',
+                    'status' => 1,
+                ],
+            ],
+        ];
+
+        $response = $this->actingAsTestUser()->postJson('/api/processflows', $processFlowData);
+        $response->assertStatus(201);
+        $processFlowId = $response->json('data.id');
+
+        $this->actingAsTestUser()->putJson('/api/processflows/' . $processFlowId, [
+            'name' => 'Test Process Flow Updated',
+        ])
+            ->assertStatus(201)->assertJsonStructure([
+            'data' => [
+                'id',
+                'name',
+                'frequency',
+                'status',
+                'frequency_for',
+                'day',
+                'week',
+                'steps' => [
+                    '*' => [
+                        'id',
+                        'name',
+                        'step_route',
+                        'assignee_user_route',
+                        'next_user_designation',
+                        'next_user_department',
+                        'next_user_unit',
+                        'next_user_location',
+                        'step_type',
+                        'user_type',
+                        'status',
+                    ],
+                ],
+            ],
+        ]);
+
+    }
+    public function test_to_update_process_flow_without_steps_successfully(): void
+    {
+        $processFlowData = ProcessFlow::factory()->create();
+        $processFlowId = $processFlowData->id;
+        $data = [
+            'name' => 'Updated Process Flow Name',
+        ];
+
+        $this->actingAsTestUser()->putJson('/api/processflows/' . $processFlowId, $data)->assertStatus(201)->assertDataBaseHas('process_flows', $data)
+            ->assertJson([
+                'name' => 'Updated Process Flow Name',
+            ]);
+
+    }
+    public function test_to_unauthorized_cannot_update_process_flow_(): void
+    {
+        $processFlowData = ProcessFlow::factory()->create();
+        $processFlowId = $processFlowData->id;
+        $data = [
+            'name' => 'Updated Process Flow Name',
+        ];
+
+        $this->putJson('/api/processflows/' . $processFlowId, $data)->assertStatus(401);
+
+    }
+
+    public function test_to_return_error_when_trying_to_update_nonexistent_process_flow(): void
+    {
+
+        $id = 9999;
+        $data = [
+            'name' => 'Updated Process Flow Name',
+        ];
+
+        $response = $this->actingAsTestUser()->putJson('/api/processflows/' . $id, $data);
+        $response->assertStatus(404);
     }
 }
