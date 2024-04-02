46b38416da2702f4a3e60bde29a9dd71a43ba38a
Collins Nwoko C <collinsnwoko05@gmail.com>
2024-04-02 11:17:36 +0100

    wip

---
+++ tests/Unit/Queue/UnitQueueTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit\Queue;
+
+use PHPUnit\Framework\TestCase;
+
+class UnitQueueTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
