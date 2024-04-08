5c780e1796e605873140906d6fd1851c4b660164
Collins Nwoko C <collinsnwoko05@gmail.com>
2024-04-08 20:44:03 +0100

    setup files

---
+++ tests/Unit/Queue/DepartmentQueueTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit\Queue;
+
+use PHPUnit\Framework\TestCase;
+
+class DepartmentQueueTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
