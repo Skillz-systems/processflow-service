9f4d8929385234d0b54c78b45e3675709fa2fa62
Collins Nwoko C <collinsnwoko05@gmail.com>
2024-04-10 08:29:28 +0100

    wip

---
+++ tests/Unit/Queue/WorkflowHistoryQueueTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit\Queue;
+
+use PHPUnit\Framework\TestCase;
+
+class WorkflowHistoryQueueTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
