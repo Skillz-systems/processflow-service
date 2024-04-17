3c758c3b7276a98bcf18b4cdae50ba825caffeb4
Collins Nwoko C <collinsnwoko05@gmail.com>
2024-04-17 18:12:27 +0100

    wip

--- tests/Unit/Queue/UserQueueTest.php
+++ tests/Unit/Queue/UserQueueTest.php
@@ -2,15 +2,61 @@
 
 namespace Tests\Unit\Queue;
 
-use PHPUnit\Framework\TestCase;
+use Tests\TestCase;
+use App\Models\User;
+use App\Service\UserService;
+use App\Jobs\User\UserCreated;
+use App\Jobs\User\UserDeleted;
+use App\Jobs\User\UserUpdated;
+use Illuminate\Support\Facades\Queue;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UserQueueTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     */
-    public function test_example(): void
+     use RefreshDatabase;
+
+    private UserService $service;
+
+    protected function setUp(): void
     {
-        $this->assertTrue(true);
+        parent::setUp();
+        // $this->service = new UserService();
+         $this->service = $this->app->make(UserService::class);
     }
+
+
+     public function test_it_dispatches_user_creation_job_functionality(): void
+    {
+        Queue::fake();
+
+        $request = [
+            "name" => "john doe",
+            "id" => 1,
+            "email" => "test@nnpc.com",
+        ];
+
+        UserCreated::dispatch($request);
+
+        Queue::assertPushed(UserCreated::class, function ($job) use ($request) {
+            return $job->getData() == $request;
+        });
+    }
+
+
+    public function test_it_dispatches_user_deletion_job_functionality(): void
+    {
+
+        Queue::fake();
+
+        $user = User::factory()->create();
+
+        UserDeleted::dispatch($user->id);
+
+        Queue::assertPushed(UserDeleted::class, function ($job) use ($user) {
+            return $job->getId() == $user->id;
+        });
+    }
+
+
+
 }
