d2a3bb4fc3b51c4664f253f70c226369dd85a911
Collins Nwoko C <collinsnwoko05@gmail.com>
2024-04-28 15:31:02 +0100

    wip:hello

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -11,12 +11,7 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-//     protected function actingAsTestUser()
-//     {
-//         $user = User::factory()->create();
 
-//         return $this->actingAs($user);
-// }
 
 public  function actingAsAuthenticatedTestUser(){
     Http::fake([
